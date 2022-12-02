2730ef1bcae1842287f82bb3f3c0dcf31301a507
joaoalberis <alberisjoao@hotmail.comm>
2022-12-02 19:50:13 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import icon from "../../assets/img/notification-icon.svg"
 import { BASE_URL } from "../../utils/request";
+import { toast } from 'react-toastify';
 
 import './style.css'
 
@@ -11,7 +12,7 @@ type Props = {
 function handleClick(id :number){
     axios.get(`${BASE_URL}/sales/${id}/notification`)
         .then(response=> {
-            console.log("sucesso");
+            toast.info("SMS Enviado com sucesso");
         });
 }
 
